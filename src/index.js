fa021478ad5ffd7d148e1d9d2d9d2ebba17b8a36
Loke-KS <24031402@myrp.edu.sg>
2025-10-27 12:28:45 +0800

    Initial commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,83 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+function App() {
+    return (
+        <div>
+            <Header />
+            <Pizza />
+            <br />
+            <Pizza2 />
+            <br />
+            <Pizza3 />
+            <br />
+            <Pizza4 />
+            <br />
+            <Pizza5 />
+            <br />
+            <Pizza6 />
+        </div>
+    );
+}
+
+function Header() {
+    return <h1>Andy's Pizza Co.</h1>
+}
+
+function Pizza() {
+    return (
+        <div>
+            <img src="pizzas/focaccia.jpg"></img>
+            <h2>Focaccia Pizza</h2>
+        </div>
+    );
+}
+
+function Pizza2() {
+    return (
+        <div>
+            <img src="pizzas/funghi.jpg"></img>
+            <h2>Funghi Pizza</h2>
+        </div>
+    );
+}
+
+function Pizza3() {
+    return (
+        <div>
+            <img src="pizzas/margherita.jpg"></img>
+            <h2>Margherita Pizza</h2>
+        </div>
+    );
+}
+
+function Pizza4() {
+    return (
+        <div>
+            <img src="pizzas/prosciutto.jpg"></img>
+            <h2>Prosciutto Pizza</h2>
+        </div>
+    );
+}
+
+function Pizza5() {
+    return (
+        <div>
+            <img src="pizzas/salamino.jpg"></img>
+            <h2>Salamino Pizza</h2>
+        </div>
+    );
+}
+
+function Pizza6() {
+    return (
+        <div>
+            <img src="pizzas/spinaci.jpg"></img>
+            <h2>Spinaci Pizza</h2>
+        </div>
+    );
+}
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+root.render(<App />);
